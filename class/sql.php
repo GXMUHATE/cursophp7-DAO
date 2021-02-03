50337da317898ca1d2d7697bf94ebc788db1191f
Graçane Muhate <gxmuhate@gmail.com>
2021-02-03 20:12:29 +0200

    Update de pastas de classes

--- class/sql.php
+++ class/sql.php
@@ -6,6 +6,7 @@ class Sql extends PDO {
     public function __construct(){
         $this->conn = new PDO("mysql:host=localhost; dbname=dbphp7", "root", "");
     }
+    
     private function setParams($statment, $parameters = array()){
         foreach($parameters as $key => $value){
             $this->setParam($key, $value);
@@ -17,6 +18,7 @@ class Sql extends PDO {
     }
 
     public function query($rawQuery, $params = array()){
+        
         $stmt = $this->conn->prepare($rawQuery);
         $this->setParams($stmt, $params);
         $stmt->execute();
